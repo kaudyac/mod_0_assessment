ab8937ff7a5e300def397a84879eb41fef12fd7b
Alexandra Kaudy <alexkaudy@gmail.com>
2021-04-02 16:52:20 -0600

    Add content to model_a.rb

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+adam_favorite_dinosaur = {
+  genus_name = "pachycephalosaurus"
+  alive_epoch = "late cretaceous"
+  num_alive_today = 0
+  average_body_length_in_meters = 5
+  is_extinct = true
+  had_soft_head = false
+  continents_inhabited = ["north america", "asia"]
+  featured_in_movies = ["Jurassic World", "The Land Before Time"]
+}
